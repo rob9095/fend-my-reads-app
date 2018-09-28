c238f9d8d7489447c14979088715e47759d6a1a8
roberttucker9095@gmail.com <robt9095@gmail.com>
2018-09-28 15:15:21 -0700

    added functionality to move books to different shelves

--- src/Homepage.js
+++ src/Homepage.js
@@ -12,96 +12,111 @@ class Homepage extends Component {
         {id: 'read', title: 'Read'}
       ],
       books: [],
-      loading: true,
     }
   }
-    componentDidMount(){
-      //fetch books
-      BooksAPI.getAll()
-      .then((books) => {
-        console.log(books)
-        this.setState({
-          books,
-          loading: false,
-        })
-      })
-      .catch(err => {
-        this.setState({
-          error: true,
-          errorMessage: 'Unable to get books, please refresh the page to try again.'
-        })
-      })
-    }
 
-    handleSelect = (id,e) => {
-      console.log(e.target.value)
-      console.log(id)
-    }
+  getBooks = () => {
+    //fetch books
+    BooksAPI.getAll()
+    .then((books) => {
+      this.setState({
+        books: books,
+      })
+    })
+    .catch(err => {
+      this.setState({
+        error: true,
+        errorMessage: 'Unable to get books, please refresh the page to try again.',
+      })
+    })
+  }
 
-    render() {
-      let { books, loading, shelves } = this.state;
-      let allShelves = {};
-      if (loading) {
-        return(
-          <div>
-            <div className="list-books">
-              <div className="loader">loading...</div>
-            </div>
-          </div>
-        )
-      }
-      for (let shelf of shelves) {
-        allShelves = {
-          ...allShelves,
-          [shelf.id]: <div className="bookshelf" key={shelf.id}>
-            <h2 className="bookshelf-title">{shelf.title}</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-                {books.filter(b => b.shelf === shelf.id).map(b => (
-                  <li key={b.id}>
-                    <div className="book">
-                      <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${b.imageLinks.thumbnail}')` }}></div>
-                        <div className="book-shelf-changer">
-                          <select name="moveSelect" onChange={(e) => this.handleSelect(b.id, e)}>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                          </select>
-                        </div>
-                      </div>
-                      <div className="book-title">{b.title}</div>
-                      <div className="book-authors">{b.authors}</div>
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </div>
-          </div>
+  componentDidMount(){
+    this.getBooks()
+  }
+
+  handleUpdate = (id,shelf) => {
+    BooksAPI.update(id,shelf)
+    .then(res => {
+      // this.getBooks();
+      let books = this.state.books.map(b => {
+        if (b.id === id) {
+          return ({
+            ...b,
+            shelf,
+          })
+        } else {
+          return({
+            ...b,
+          })
         }
-      }
-      return(
-        <div>
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                {allShelves.currentlyReading}
-                {allShelves.wantToRead}
-                {allShelves.read}
-              </div>
-            </div>
-            <div className="open-search">
-              <Link to="/search">Add a book</Link>
-            </div>
+      })
+      this.setState({books})
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
+  handleSelect = (id,e) => {
+    this.handleUpdate(id,e.target.value)
+  }
+
+  render() {
+    let { books, loading, shelves } = this.state;
+    let allShelves = {};
+    for (let shelf of shelves) {
+      allShelves = {
+        ...allShelves,
+        [shelf.id]: <div className="bookshelf" key={shelf.id}>
+          <h2 className="bookshelf-title">{shelf.title}</h2>
+          <div className="bookshelf-books">
+            <ol className="books-grid">
+              {books.filter(b => b.shelf === shelf.id).sort((a,b)=>(a.title - b.title)).map(b => (
+                <li key={b.id}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${b.imageLinks.thumbnail}')` }}></div>
+                      <div className="book-shelf-changer">
+                        <select value="move" onChange={(e) => this.handleSelect(b.id, e)}>
+                          <option value="move" disabled>Move to...</option>
+                          <option value="currentlyReading">Currently Reading</option>
+                          <option value="wantToRead">Want to Read</option>
+                          <option value="read">Read</option>
+                          <option value="none">None</option>
+                        </select>
+                      </div>
+                    </div>
+                    <div className="book-title">{b.title}</div>
+                    <div className="book-authors">{b.authors}</div>
+                  </div>
+                </li>
+              ))}
+            </ol>
           </div>
         </div>
-      )
+      }
     }
+    return(
+      <div>
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              {allShelves.currentlyReading}
+              {allShelves.wantToRead}
+              {allShelves.read}
+            </div>
+          </div>
+          <div className="open-search">
+            <Link to="/search">Add a book</Link>
+          </div>
+        </div>
+      </div>
+    )
   }
+}
 
 export default Homepage
