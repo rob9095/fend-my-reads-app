ade8eb523d61a841e96eeade919e0b29be173bc3
roberttucker9095@gmail.com <robt9095@gmail.com>
2018-09-28 17:36:32 -0700

    updates to search page to meet most critera on rubric, still ned to show none option select option for books that aren't on a shelf yet

--- src/BookListItem.js
+++ src/BookListItem.js
@@ -11,14 +11,24 @@ class BookListItem extends Component {
     this.props.onSelect(this.props.book.id,e.target.value)
   }
   render() {
-    let {id, imageLinks, title, authors} = this.props.book
+    let {id, imageLinks, title, authors, shelf} = this.props.book
+    //set authors to N/A if undefined otherwise comma seperate authors
+    authors = authors === undefined ? 'N/A' : authors.map((a,i)=>{
+      if(i === authors.length-1) {
+        return a
+      } else {
+        return `${a}, `
+      }
+    })
+    //set placeholder image if thumbnail is undefined
+    let image = imageLinks === undefined ? 'url("/images/placeholder.png")' : `url('${imageLinks.thumbnail}')`;
     return(
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url('${imageLinks.thumbnail}')` }}></div>
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: image }}></div>
             <div className="book-shelf-changer">
-              <select value="move" onChange={this.handleSelect}>
+              <select value={shelf} onChange={this.handleSelect}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
--- src/Search.js
+++ src/Search.js
@@ -1,47 +1,125 @@
 import React, { Component } from 'react';
-import { search } from './BooksAPI';
+import { Link } from 'react-router-dom';
+import BookListItem from './BookListItem';
+import { search, update, getAll } from './BooksAPI';
 
 class Search extends Component {
   constructor(props) {
     super(props)
     this.state = {
       searchValue: '',
+      books: [],
+      error: false,
+      errorMessage: '',
+      currentBooks: [],
     }
 
   }
+
+  getBooks = () => {
+    //fetch books
+    getAll()
+    .then((books) => {
+      this.setState({
+        currentBooks: books,
+      })
+    })
+    .catch(err => {
+      this.setState({
+        error: true,
+        errorMessage: 'Unable to get books, please refresh the page to try again.',
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getBooks()
+  }
+
   handleSearch = (e) => {
+    //clear errors set state for new input
     this.setState({
       [e.target.name]: e.target.value,
+      error: false,
     })
+    //if search value is empty return
+    if (e.target.value === '') {
+      this.setState({
+        error: true,
+        errorMessage: `Please enter a search value`
+      })
+      return
+    }
+    // search books API
     search(e.target.value)
-    .then(res => {
-      console.log(res)
+    .then(books => {
+      if (books.error) {
+        this.setState({
+          error: true,
+          errorMessage: `No Books Found`,
+        })
+      } else {
+        console.log(books)
+        this.setState({
+          books,
+        })
+      }
     })
     .catch(err => {
       console.log(err)
     })
   }
+
+  handleUpdate = (id,shelf) => {
+    update(id,shelf)
+    .then(res => {
+    })
+    .catch(err=>{
+      console.log(err)
+    })
+  }
+
   render() {
-    const { searchValue } = this.state;
+    const { searchValue, books, error, errorMessage, currentBooks } = this.state;
+    let results = [];
+    if (books.length > 0) {
+      results = books.sort((a,b)=>(a.title - b.title)).map(b => {
+        let currentBook = currentBooks.find(cb=>cb.id === b.id)
+        if (currentBook) {
+          let book = {
+            ...b,
+            shelf: currentBook.shelf,
+          }
+          return (
+            <BookListItem
+              key={b.id}
+              book={book}
+              onSelect={this.handleUpdate}
+            />
+          )
+        } else {
+          return(
+            <BookListItem
+              key={b.id}
+              book={b}
+              onSelect={this.handleUpdate}
+            />
+          )
+        }
+      })
+    }
     return(
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
             <input type="text" name="searchValue" value={searchValue} onChange={this.handleSearch} placeholder="Search by title or author"/>
-
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {error ? <span>{errorMessage}</span> : results}
+          </ol>
         </div>
       </div>
     )
